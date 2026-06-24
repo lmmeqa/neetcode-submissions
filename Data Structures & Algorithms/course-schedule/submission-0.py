2ab464579fe22472ef2a9b142ea68ebc21d8703b
lmmeqa <128739579+lmmeqa@users.noreply.github.com>
2026-06-23 19:49:01 -0700

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,26 @@
+from collections import defaultdict, deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        sequence = [-1]*numCourses
+        tree = defaultdict(list)
+        numprereqs = [0]*numCourses
+        for prereq in prerequisites:
+            tree[prereq[1]].append(prereq[0])
+            numprereqs[prereq[0]] += 1
+        queue = deque()
+        for course, pending in enumerate(numprereqs):
+            if pending == 0:
+                queue.append(course)
+        while queue:
+            taken = queue.popleft()
+            for course in tree[taken]:
+                numprereqs[course]-=1
+                if numprereqs[course] == 0: queue.append(course)
+        return not any(numprereqs)
+
+
+        
+
+
+
+        
