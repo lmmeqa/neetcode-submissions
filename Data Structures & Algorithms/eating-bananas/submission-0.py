d80d4d91bdbb25c93ae6058ee1c9f79b119ac129
lmmeqa <128739579+lmmeqa@users.noreply.github.com>
2026-06-23 22:54:49 -0700

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,31 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def caneat(speed:int):
+            hours = 0
+            for pile in piles:
+                hours+=math.ceil(pile/speed)
+            if hours>h:
+                return False
+            return True
+
+        low = 1
+        high = max(piles)
+
+        while low!=high:
+            mid = (high+low)//2
+            if caneat(mid):
+                high = mid
+            else:
+                low = mid + 1
+
+        return high
+
+            
+        
+            
+
+        
+            
+
+
