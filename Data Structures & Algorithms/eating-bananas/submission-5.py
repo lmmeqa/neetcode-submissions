40551213bce9cd3bbb6aaada13f74a52fc58a271
lmmeqa <128739579+lmmeqa@users.noreply.github.com>
2026-07-07 21:09:46 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def feasible(k):
+            hours = 0
+            for pile in piles:
+                hours+=-1*(pile//(-1*k))
+            return hours<=h
+        
+        hi = max(piles)
+        lo = 1
+        while lo < hi:
+            mid = (lo + hi)//2
+            if feasible(mid):
+                hi = mid
+            else:
+                lo = mid+1
+        return hi
