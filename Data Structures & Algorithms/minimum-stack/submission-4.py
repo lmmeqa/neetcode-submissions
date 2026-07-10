3174ce705c5b041a28d10ce2ce92e54e623bca41
lmmeqa <128739579+lmmeqa@users.noreply.github.com>
2026-07-09 17:18:12 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.levelMins = {}
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.stack) == 1:
+            self.levelMins[len(self.stack)] = val
+        else:
+            self.levelMins[len(self.stack)] = min(val, self.levelMins[len(self.stack)-1])
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.levelMins[len(self.stack)]
+
+        
