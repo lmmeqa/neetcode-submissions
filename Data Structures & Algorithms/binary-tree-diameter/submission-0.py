152dd14a8dfaebfddeb309621c5dde7980f6b558
lmmeqa <128739579+lmmeqa@users.noreply.github.com>
2026-07-09 18:45:18 -0700

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maxdiameter = 0
+        def longestpathtoleaf(root: Optional[TreeNode]):
+            nonlocal maxdiameter
+            if root.right:
+                rightpath = 1 + longestpathtoleaf(root.right)
+            else:
+                rightpath = 0
+            if root.left:
+                leftpath = 1 + longestpathtoleaf(root.left)
+            else:
+                leftpath = 0
+            maxdiameter = max(leftpath+rightpath, maxdiameter)
+            return max(leftpath,rightpath)
+        rightpath = 0
+        leftpath = 0
+        if root.right:   
+            rightpath = 1 + longestpathtoleaf(root.right)
+        if root.left:
+            leftpath = 1 + longestpathtoleaf(root.left)
+        return max(leftpath+rightpath, maxdiameter)
+
+        
+
+
+
+        
